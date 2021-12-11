07d26dd3af7f0b95bfd68ac5b872287042a60efd
transang832001 <78015853+transang832001@users.noreply.github.com>
2021-12-11 15:17:26 +0700

    Add files via upload

---
+++ movie-recommendation-system/MovieRecommendationSystem/Infrastructure/CsvMovieMapping.cs
@@ -0,0 +1,15 @@
+﻿using MovieRecommendationSystem.Models;
+using TinyCsvParser.Mapping;
+
+namespace MovieRecommendationSystem.Infrastructure
+{
+    public class CsvMovieMapping : CsvMapping<Movie>
+    {
+        public CsvMovieMapping() : base()
+        {
+            MapProperty(0, x => x.MovieId);
+            MapProperty(1, x => x.UserId);
+            MapProperty(2, x => x.Rating);
+        }
+    }
+}
